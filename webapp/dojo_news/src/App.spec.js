0046774d1f36476e01258f722f3f928b9060be37
Donat Deva Brzoska <donat.brzoska@gmail.com>
2020-11-07 20:04:58 +0100

    divide tests into sections

--- webapp/dojo_news/src/App.spec.js
+++ webapp/dojo_news/src/App.spec.js
@@ -3,163 +3,167 @@ import App from "./App";
 import 'regenerator-runtime';
 import Vue from "vue";
 
-const listEmptyMessage = "Oh noes, the list is empty!";
+describe("List Empty Message", () => {
+    const listEmptyMessage = "Oh noes, the list is empty!";
 
-it('displays a message on an initially empty list of NewsItems', () => {
-    const wrapperWithNonEmptyList = shallowMount(App, {
-        propsData: {
-            listEmptyMessage: listEmptyMessage,
-            initialNewsListItems: []
-        }
+    it('displays a message on an initially empty list of NewsItems', () => {
+        const wrapperWithNonEmptyList = shallowMount(App, {
+            propsData: {
+                listEmptyMessage: listEmptyMessage,
+                initialNewsListItems: []
+            }
+        });
+        expect(wrapperWithNonEmptyList.text()).toContain(listEmptyMessage);
+    })
+
+    it('displays no message on an initially non-empty list of NewsItems', () => {
+        const wrapper = shallowMount(App, {
+            propsData: {
+                listEmptyMessage: listEmptyMessage,
+                initialNewsListItems: [
+                    {id: 0, title: "macOS", votes: 0},
+                    {id: 1, title: "Linux", votes: 0},
+                    {id: 2, title: "Windows", votes: 0}
+                ]
+            }
+        });
+
+        expect(wrapper.text()).not.toContain(listEmptyMessage);
     });
-    expect(wrapperWithNonEmptyList.text()).toContain(listEmptyMessage);
-})
 
-it('displays no message on an initially non-empty list of NewsItems', () => {
-    const wrapper = shallowMount(App, {
-        propsData: {
-            listEmptyMessage: listEmptyMessage,
-            initialNewsListItems: [
-                {id: 0, title: "macOS", votes: 0},
-                {id: 1, title: "Linux", votes: 0},
-                {id: 2, title: "Windows", votes: 0}
-            ]
-        }
+    it('adds and removes the message on list mutations', async () => {
+        const wrapper = shallowMount(App, {
+            propsData: {
+                listEmptyMessage: listEmptyMessage,
+                initialNewsListItems: []
+            }
+        });
+
+        expect(wrapper.text()).toContain(listEmptyMessage);
+
+        // alternatively, call .vm.$emit
+        wrapper.vm.createNewsListItem("Test")
+
+        await Vue.nextTick();
+
+        expect(wrapper.text()).not.toContain(listEmptyMessage);
+
+        // alternatively, call .vm.$emit
+        wrapper.vm.removeNewsListItem(0);
+
+        await Vue.nextTick();
+
+        expect(wrapper.html()).toContain(listEmptyMessage);
     });
-
-    expect(wrapper.text()).not.toContain(listEmptyMessage);
 });
 
-it('adds and removes the message on list mutations', async () => {
-    const wrapper = shallowMount(App, {
-        propsData: {
-            listEmptyMessage: listEmptyMessage,
-            initialNewsListItems: []
-        }
+describe("Reverse Order Button", () => {
+    it('list order is reversed upon Reverse-Button click from initial state _descending_', async () => {
+        const wrapper = shallowMount(App, {
+            propsData: {
+                initialNewsListItems: [
+                    {id: 0, title: "macOS", votes: 4},
+                    {id: 1, title: "Linux", votes: 8},
+                    {id: 2, title: "centOS", votes: 3},
+                    {id: 3, title: "Windows", votes: 0}
+                ],
+                initialDescendingOrder: true
+            }
+        });
+
+        let expectedIdList = [1, 0, 2, 3];
+        let actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
+        expect(expectedIdList).toEqual(actualIdList);
+
+        await wrapper.find("#reverse-order-button").trigger("click");
+        expectedIdList = expectedIdList.reverse();
+        actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
+        expect(expectedIdList).toEqual(actualIdList);
     });
 
-    expect(wrapper.text()).toContain(listEmptyMessage);
+    it('list order is reversed upon Reverse-Button click from initial state _ascending_', async () => {
+        const wrapper = shallowMount(App, {
+            propsData: {
+                initialNewsListItems: [
+                    {id: 0, title: "macOS", votes: 4},
+                    {id: 1, title: "Linux", votes: 8},
+                    {id: 2, title: "centOS", votes: 3},
+                    {id: 3, title: "Windows", votes: 0}
+                ],
+                initialDescendingOrder: false
+            }
+        });
 
-    // alternatively, call .vm.$emit
-    wrapper.vm.createNewsListItem("Test")
+        let expectedIdList = [3, 2, 0, 1];
+        let actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
+        expect(expectedIdList).toEqual(actualIdList);
 
-    await Vue.nextTick();
-
-    expect(wrapper.text()).not.toContain(listEmptyMessage);
-
-    // alternatively, call .vm.$emit
-    wrapper.vm.removeNewsListItem(0);
-
-    await Vue.nextTick();
-
-    expect(wrapper.html()).toContain(listEmptyMessage);
-});
-
-it('list order is reversed upon Reverse-Button click from initial state _descending_', async () => {
-    const wrapper = shallowMount(App, {
-        propsData: {
-            initialNewsListItems: [
-                {id: 0, title: "macOS", votes: 4},
-                {id: 1, title: "Linux", votes: 8},
-                {id: 2, title: "centOS", votes: 3},
-                {id: 3, title: "Windows", votes: 0}
-            ],
-            initialDescendingOrder: true
-        }
+        await wrapper.find("#reverse-order-button").trigger("click");
+        expectedIdList = expectedIdList.reverse();
+        actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
+        expect(expectedIdList).toEqual(actualIdList);
     });
 
-    let expectedIdList = [1, 0, 2, 3];
-    let actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
-    expect(expectedIdList).toEqual(actualIdList);
+    it('displays the Reverse-Order-Button on an initially non empty list of NewsItems', () => {
+        const wrapper = shallowMount(App, {
+            propsData: {
+                initialNewsListItems: [
+                    {id: 0, title: "macOS", votes: 0},
+                    {id: 1, title: "Linux", votes: 0},
+                    {id: 2, title: "Windows", votes: 0}
+                ]
+            }
+        });
 
-    await wrapper.find("#reverse-order-button").trigger("click");
-    expectedIdList = expectedIdList.reverse();
-    actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
-    expect(expectedIdList).toEqual(actualIdList);
-});
-
-it('list order is reversed upon Reverse-Button click from initial state _ascending_', async () => {
-    const wrapper = shallowMount(App, {
-        propsData: {
-            initialNewsListItems: [
-                {id: 0, title: "macOS", votes: 4},
-                {id: 1, title: "Linux", votes: 8},
-                {id: 2, title: "centOS", votes: 3},
-                {id: 3, title: "Windows", votes: 0}
-            ],
-            initialDescendingOrder: false
-        }
+        expect(wrapper.text()).toContain("Reverse Order");
     });
 
-    let expectedIdList = [3, 2, 0, 1];
-    let actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
-    expect(expectedIdList).toEqual(actualIdList);
+    it('does not display the Reverse-Order-Button on an initially empty list of NewsItems', () => {
+        const wrapperWithEmptyList = shallowMount(App, {
+            propsData: {
+                initialNewsListItems: []
+            }
+        });
 
-    await wrapper.find("#reverse-order-button").trigger("click");
-    expectedIdList = expectedIdList.reverse();
-    actualIdList = wrapper.vm.sortedNewsListItems.map(x => x.id);
-    expect(expectedIdList).toEqual(actualIdList);
-});
-
-it('displays the Reverse-Order-Button on an initially non empty list of NewsItems', () => {
-    const wrapper = shallowMount(App, {
-        propsData: {
-            initialNewsListItems: [
-                {id: 0, title: "macOS", votes: 0},
-                {id: 1, title: "Linux", votes: 0},
-                {id: 2, title: "Windows", votes: 0}
-            ]
-        }
+        expect(wrapperWithEmptyList.text()).not.toContain("Reverse Order");
     });
 
-    expect(wrapper.text()).toContain("Reverse Order");
-});
+    it('Reverse-Button is hidden upon empty list', async () => {
+        const wrapper = mount(App, {
+            propsData: {
+                initialNewsListItems: [
+                    {id: 0, title: "macOS", votes: 0},
+                    {id: 1, title: "Linux", votes: 0},
+                    {id: 2, title: "Windows", votes: 0}
+                ]
+            }
+        });
+        expect(wrapper.text()).toContain("Reverse Order");
 
-it('does not display the Reverse-Order-Button on an initially empty list of NewsItems', () => {
-    const wrapperWithEmptyList = shallowMount(App, {
-        propsData: {
-            initialNewsListItems: []
+        let buttons = wrapper.findAll("button").wrappers;
+        let removeButtons = buttons.filter(x => x.text().includes("Remove"));
+        for (let removeButton of removeButtons) {
+            await removeButton.trigger("click");
         }
+        // or we just do:
+        // wrapper.vm.removeNewsListItem(0);
+        // await Vue.nextTick();
+        // but we don't, because the current implementation took too long to figure out
+
+        expect(wrapper.text()).not.toContain("Reverse Order");
     });
 
-    expect(wrapperWithEmptyList.text()).not.toContain("Reverse Order");
-});
+    it('Reverse-Button shows up again upon non-empty list', async () => {
+        const wrapperWithEmptyList = shallowMount(App, {
+            propsData: {
+                initialNewsListItems: []
+            }
+        });
+        expect(wrapperWithEmptyList.text()).not.toContain("Reverse Order");
 
-it('Reverse-Button is hidden upon empty list', async () => {
-    const wrapper = mount(App, {
-        propsData: {
-            initialNewsListItems: [
-                {id: 0, title: "macOS", votes: 0},
-                {id: 1, title: "Linux", votes: 0},
-                {id: 2, title: "Windows", votes: 0}
-            ]
-        }
+        wrapperWithEmptyList.vm.createNewsListItem("Test");
+        await Vue.nextTick();
+
+        expect(wrapperWithEmptyList.text()).toContain("Reverse Order");
     });
-    expect(wrapper.text()).toContain("Reverse Order");
-
-    let buttons = wrapper.findAll("button").wrappers;
-    let removeButtons = buttons.filter(x => x.text().includes("Remove"));
-    for (let removeButton of removeButtons) {
-        await removeButton.trigger("click");
-    }
-    // or we just do:
-    // wrapper.vm.removeNewsListItem(0);
-    // await Vue.nextTick();
-    // but we don't, because the current implementation took too long to figure out
-
-    expect(wrapper.text()).not.toContain("Reverse Order");
-});
-
-it('Reverse-Button shows up again upon non-empty list', async () => {
-    const wrapperWithEmptyList = shallowMount(App, {
-        propsData: {
-            initialNewsListItems: []
-        }
-    });
-    expect(wrapperWithEmptyList.text()).not.toContain("Reverse Order");
-
-    wrapperWithEmptyList.vm.createNewsListItem("Test");
-    await Vue.nextTick();
-
-    expect(wrapperWithEmptyList.text()).toContain("Reverse Order");
 });
